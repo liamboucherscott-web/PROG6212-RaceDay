bc98aeef05426ddb02237d75efd7862c0d2d9309
liamboucherscott-web <liam.boucher.scott@gmail.com>
2026-09-22 09:19:25 +0200

    docs: add SQL database header

--- docs/RaceDayDB.sql
+++ docs/RaceDayDB.sql
@@ -1,2 +1,16 @@
--- RaceDay Database Script
--- This script is in progress. Full schema and seed data to follow.
+- RaceDay Database Script
+-- PROG6212 - Programming 2B
+-- Liam Scott - ST10467183
+
+-- Drop the database if it already exists
+IF DB_ID('RaceDayDB') IS NOT NULL
+    DROP DATABASE RaceDayDB;
+GO
+
+-- Create the database
+CREATE DATABASE RaceDayDB;
+GO
+
+-- Use the database
+USE RaceDayDB;
+GO
